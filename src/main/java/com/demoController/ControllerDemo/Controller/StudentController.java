f1d7e18da1d2f83ea8b6d7dee8c92a76f9b0e133
koheinthurako <captainthura99@gmail.com>
2024-01-19 23:53:03 +0630

    noted about RequestController, RequestMapping, GetMapping

--- src/main/java/com/demoController/ControllerDemo/Controller/StudentController.java
+++ src/main/java/com/demoController/ControllerDemo/Controller/StudentController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//	@RestController = @Controller + @ResponseBody
+//	@RestController က frontEnd & backEnd ကို ကြားထဲကလှမ်းချိတ်ပေးဖို့ spring က ရေးပေးထားတဲ့ feature ဖြစ်တယ်
+//	သူကအောက်က sentMsg() ကိုလှမ်းခေါ်လိုက်တာနဲ့ Server ကနေ Browser မှာ return ပြန်ထားတာကို လှမ်းပြပေးနိုင်တယ်
+//	လှမ်းခေါ်တဲ့နည်းလမ်းကတော့ အောက်က @RequestMapping နဲ့ခေါ်တယ် () parameter ထဲက value မှာ ကိုယ်စားပြုနာမည်ကိုထည့်ပေးရတယ်
+//	method မှာ အောက်ကလို ဘာမှမထည့်ရင်တော့ default က ReqeustMethod.GET အနေနဲ့အလုပ်လုပ်တယ်
 @RequestMapping("/std")
-//@RequestMapping က Server ဘက်က data ကို client ဘက် (Browser) ကို လှမ်းပို့ခြင်း သူ့ကို class, method level မှာသုံးလို့ရတယ်
+//	@RequestMapping က Server ဘက်က data ကို client ဘက် (Browser) ကို လှမ်းပို့ခြင်း သူ့ကို class, method level မှာသုံးလို့ရတယ်
 public class StudentController {
 	
 //	server ဘက်ကနေ client ကိုပို့ရင် GET method()
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="", method=RequestMethod.GET)
 	public String sentMsg() {
 		return "This is home page";
 	}
@@ -22,6 +27,7 @@ public class StudentController {
 	}
 	
 //	@GetMapping က အပေါ်ကရေးထားတဲ့ RequestMapping ပုံစံအလုပ်လုပ်တာနဲ့ အတူတူပဲ
+//	နောက်ပိုင်းမှာတော့ @GetMapping ကိုပဲ အသုံးများတယ်
 	@GetMapping("/mgp")
 	public String sentMsg3() {
 		return "Mingalarpar";
--- src/main/java/com/demoController/ControllerDemo/ControllerDemoApplication.java
+++ src/main/java/com/demoController/ControllerDemo/ControllerDemoApplication.java
@@ -3,6 +3,9 @@ package com.demoController.ControllerDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//	Spring Controller အကြောင်းစမယ် 
+//	resources folder ထဲက application.properties ထဲမှာ ကိုယ် run ချင်တဲ့ server port နဲ့ ချိတ်ချင်တဲ့ library တွေကို သွားရေးရတယ်
+
 @SpringBootApplication
 public class ControllerDemoApplication {
 
